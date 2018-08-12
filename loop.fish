e781f3f2b3fc6014393a63f0bcf32d649e3bd413
rien333 <rien334@gmail.com>
2018-08-12 23:17:13 +0200

    Ugly getter shit

--- loop.fish
+++ loop.fish
@@ -2,5 +2,6 @@
 
 while true
     python screenshot.py
-    python -m scripts.label_image --graph=tf_files/retrained_graph.pb --image=/tmp/tinder_screenshot.png
+    python -m scripts.label_image --graph=tf_files/retrained_graph.pb --image=/tmp/tinder_screenshot.png > /tmp/tinder_scores.txt
+    python uglyparse.py /tmp/tinder_scores.txt
 end
