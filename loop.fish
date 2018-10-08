c779883f22903ff9c5b0303d80791e6437a12bf5
rien333 <rien334@gmail.com>
2018-10-08 23:15:49 +0200

    Add random sleep interval

--- loop.fish
+++ loop.fish
@@ -4,4 +4,8 @@ while true
     python screenshot.py
     python -m scripts.label_image --graph=tf_files/retrained_graph.pb --image=/tmp/tinder_screenshot.png > /tmp/tinder_scores.txt
     python uglyparse.py /tmp/tinder_scores.txt
+    # sometimes randomly sleep
+    if random 0 1 | grep 1 > /dev/null
+        sleep random 0 5
+    end
 end
