c0639f01c9c7abad8ca0e2091aa2fddbe98f2d90
rien333 <rien334@gmail.com>
2018-10-08 23:41:58 +0200

    add multiple preferences

--- uglyparse.py
+++ uglyparse.py
@@ -1,7 +1,8 @@
 import sys
 
 # Change this for the computer's preference
-preference = "sunflowers"
+# Swipe right when one of these preferences is met
+preferences = ["sunflowers", "roses", "lilies"]
 
 out_f = sys.argv[1]
 # keys: class names values: scores
@@ -13,16 +14,12 @@ with open(out_f, "r") as f:
             c = l.split("=")[0].split(" ")[0]
             scores[c] = s
 
-pref_score = scores[preference]
+pref_scores = [scores[pref] for pref in preferences]
 for c, s in scores.items():
-    if c == preference:
-        continue
-    if s > pref_score:
+    if c in preferences:
+        continue # skip
+    # check wether a non prefered class scores higher than any of the prefered classes
+    if any(s > s1 for s1 in pref_scores)
         print("Swipe left!")
         exit(0)
 print("Swipe right!")
-        
-        
-        
-
-    
